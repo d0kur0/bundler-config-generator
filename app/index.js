d9c63cf8c5fab17da27ac65d7310f9da0672b883
d0kur0 <dev.dokuro@gmail.com>
2019-08-22 02:48:37 +0300

    Добавлена проверка текущей директории на наличие файло, если они есть - установка прерывается

--- app/index.js
+++ app/index.js
@@ -1,7 +1,29 @@
 const prompts = require('prompts');
+const path    = require('path');
+const fs      = require('fs');
+const log     = console.log;
+const chalk   = require('chalk');
 
 (async () => {
 	const response = await prompts(require('./questions'));
 
-	console.log(response);
+	const currentDir = path.parse(__dirname).dir;
+	fs.readdir(currentDir, (err, files) => {
+		if (err) {
+			log(chalk.red('❌ Не удалось прочитать текущую директорию'));
+			process.exit();
+		}
+
+		if (files.length) {
+			log(chalk.red('❌ Текущая директория не пуста'));
+			process.exit();
+		}
+	});
+
+	const packageJSON = {
+		name: response.name,
+		version: "1.0.0",
+		description: "",
+		main: "index.js"
+	};
 })();

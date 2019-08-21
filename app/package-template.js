6bc7d5dcc8a23008f39008f61230cf42e08d7383
d0kur0 <dev.dokuro@gmail.com>
2019-08-22 02:49:05 +0300

    Создан шаблон для будущего package.json

---
+++ app/package-template.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "",
+	version: "1.0.0",
+	description: "",
+	main: "index.js",
+	scripts: {
+		"build": "gulp"
+	},
+	author: "",
+	license: "ISC",
+	dependencies: {
+
+	}
+}

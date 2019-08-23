5154f5a23b165b14c367573a228bb6b68f037c90
d0kur0 <dev.dokuro@gmail.com>
2019-08-23 10:14:46 +0300

    Перенёс файл описания зависимостей

---
+++ app/package.json/dependencies.js
@@ -0,0 +1,14 @@
+module.exports = [
+	{ name: "gulp",              isGlobal: false },
+	{ name: "gulp-cli",          isGlobal: true },
+	{ name: "gulp-plumber",      isGlobal: false },
+	{ name: "gulp-sourcemap",    isGlobal: false },
+	{ name: "gulp-changed",      isGlobal: false },
+	{ name: "webpack-stream",    isGlobal: false },
+	{ name: "del",               isGlobal: false },
+	{ name: "@babel/core",       isGlobal: false },
+	{ name: "@babel/preset-env", isGlobal: false },
+	{ name: "babel-loader",      isGlobal: false },
+	{ name: "browser-sync",      isGlobal: false },
+	{ name: "cross-env",         isGlobal: false },
+];

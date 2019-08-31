e651cd2b0bc5477c0c900f2e4dc914e15824e8ff
dokuro <dev.dokuro@gmail.com>
2019-08-31 23:57:57 +0300

    Модуль установки пакетов для сборщика

---
+++ app/installer.js
@@ -0,0 +1,48 @@
+const log = console.log;
+const chalk = require('chalk');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const ora = require('ora');
+
+module.exports = async (response) => {
+    const requiredPackages = require('./package.json/dependencies');
+    const globalPackages = requiredPackages.filter(p => p.isGlobal).map(p => p.name);
+    const localPackages = requiredPackages.filter(p => !p.isGlobal).map(p => p.name);
+
+    let spinner = ora({
+        text: chalk.green('Установка глобальных пакетов'),
+        spinner: require('./spinner')
+    }).start();
+
+    if (!await exec(`npm i -D -g ${globalPackages.join(' ')}`)) {
+        throw new Error("Произошла ошибка при установке пакетов");
+    }
+
+    spinner.succeed();
+
+    const linterPackages = response.linter
+        ? response.transpilation.name === 'babel'
+            ? ['eslint-loader', 'babel-eslint']
+            : ['tslint', 'tslint-loader']
+        : [];
+
+    const packagesList = [
+        ...localPackages,
+        ...response.plugins,
+        ...response.transpilation.packages,
+        ...response.styles.packages,
+        ...response.html.packages,
+        ...linterPackages
+    ];
+
+    spinner = ora({
+        text: chalk.green('Установка локальных пакетов'),
+        spinner: require('./spinner')
+    }).start();
+
+    if (!await exec(`npm i -D ${packagesList.join(' ')}`)) {
+        throw new Error("Произошла ошибка при установке пакетов");
+    }
+
+    spinner.succeed();
+};

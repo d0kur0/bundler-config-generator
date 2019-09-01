dd0a45033aa3503984165a3c512d2961f2bf5c7e
dokuro <dev.dokuro@gmail.com>
2019-09-02 00:39:11 +0300

    Добавлена реализация создания папок и файлов

--- app/mk-struct.js
+++ app/mk-struct.js
@@ -1,8 +1,19 @@
-const log = console.log;
 const chalk = require('chalk');
 const fs = require('fs');
 const ora = require('ora');
 
 module.exports = () => {
-    
+    const dirs = [
+        'src', 'src/styles', 'src/js', 'src/templates'
+    ];
+
+    const spinner = ora({
+        text: chalk.green('Создание структуры файлов и папок'),
+        spinner: require('./spinner')
+    }).start();
+
+    dirs.forEach(d => fs.mkdirSync(d));
+    fs.writeFileSync('src/js/index.js', '');
+
+    spinner.succeed();
 };

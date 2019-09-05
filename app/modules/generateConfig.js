64ccab3eebcc25a270877fa5a4600a501c3bc429
dokuro <dev.dokuro@gmail.com>
2019-09-05 04:56:26 +0300

    Новая логика генерации конфигурации путм вставки новых строк, а не замены, изменил beautify на prettier

--- app/modules/generateConfig.js
+++ app/modules/generateConfig.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const fs = require('fs');
 const ora = require('ora');
 const prettier = require("prettier");
+const appendAfter = require('../helpers/appendStringBeforeSubstring');
 
 module.exports = (response) => {
     let spinner = ora({
@@ -11,6 +12,38 @@ module.exports = (response) => {
     }).start();
 
     let gulpfile = fs.readFileSync(`${__dirname}/../config-templates/gulpfile.js`, 'utf8');
+
+    const appendTable = {
+        '//{CSS_BEFORE_COMMAND}': [
+            ...response.styles.beforeCommand,
+            ...response.plugins.filter(p => p.for === 'styles').map(p => p.beforeCommand)
+        ],
+        '//{CSS_PIPE_COMMAND}': [
+            ...response.styles.pipeCommand,
+            ...response.plugins.filter(p => p.for === 'styles').map(p => p.pipeCommand)
+        ],
+
+    };
+
+    const CSS_BEFORE_COMMANDS = [
+        ...response.styles.beforeCommand,
+        ...response.plugins.filter(p => p.for === 'styles').map(p => p.beforeCommand)
+    ];
+
+    const CSS_PIPE_COMMANDS = [
+        ...response.styles.pipeCommand,
+        ...response.plugins.filter(p => p.for === 'styles').map(p => p.pipeCommand)
+    ];
+
+    const HTML_BEFORE_COMMANDS = [
+        ...response.html.beforeCommand,
+        ...response.plugins.filter(p => p.for === 'html').map(p => p.beforeCommand)
+    ];
+
+    const HTML_PIPE_COMMANDS = [
+        ...response.html.pipeCommand,
+        ...response.plugins.filter(p => p.for === 'html').map(p => p.pipeCommand)
+    ];
 
 
 

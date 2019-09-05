2720d9b5a690e9fcf279277c9bd7bc5b8c9c4b9d
dokuro <dev.dokuro@gmail.com>
2019-09-05 04:54:29 +0300

    Изменил строки-шаблоны на комментарии

--- app/config-templates/gulpfile.js
+++ app/config-templates/gulpfile.js
@@ -4,13 +4,13 @@ const plumber = require('gulp-plumber');
 task('clear', async () => await require('del')('./dist'));
 
 task('build:CSS', () => {
-	{CSS_BEFORE_COMMAND}
+	//{CSS_BEFORE_COMMAND}
 	const sourcemaps = require('gulp-sourcemaps');
 
 	return src('./src/styles/index.css')
 		.pipe(plumber())
 		.pipe(require('gulp-if')(process.env.DEBUG, sourcemaps.init()))
-		{CSS_PIPE_COMMAND}
+		//{CSS_PIPE_COMMAND}
 		.pipe(require('gulp-if')(process.env.DEBUG, sourcemaps.write()))
 		.pipe(dest('./dist'));
 });
@@ -23,21 +23,23 @@ task('build:JS', () => {
 });
 
 task('build:HTML', () => {
-	{HTML_BEFORE_COMMAND}
+	//{HTML_BEFORE_COMMAND}
 	return src('./src/templates/pages/**')
 		.pipe(plumber())
-		{HTML_PIPE_COMMAND}
+		//{HTML_PIPE_COMMAND}
 		.pipe(dest('./dist'));
 });
 
 task('watch', () => {
 	const bSync = require('browser-sync').init({server: './dist'});
+
 	watch('./src/styles/**',    series('build:CSS'));
 	watch('./src/js/**',        series('build:JS'));
 	watch('./src/templates/**', series('build:HTML'));
+
 	watch('./dist/**').on('change', bSync.reload);
 });
 
-task('build', series('clear', parallel('build:CSS', 'build:JS', 'build:HTML', 'assets:SVG')));
+task('build', series('clear', parallel('build:CSS', 'build:JS', 'build:HTML')));
 task('default', series('build'));
 task('serve', series('build', 'watch'));

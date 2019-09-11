205b5c55ab3e9eaea22fb21e3077a74e93dde065
d0kur0 <dev.dokuro@gmail.com>
2019-09-11 18:01:11 +0300

    Change color from logs, remove comments

--- app/index.js
+++ app/index.js
@@ -8,14 +8,14 @@ const chalk = require('chalk');
 	const response = await prompts(require('./questions'));
 
 	try {
-		//await require('./modules/initNPM')(response.name);
-		//await require('./modules/installPackages')(response);
-		//require('./modules/makeStruct')();
+		await require('./logics/initNPM')(response.name);
+		await require('./logics/installPackages')(response);
+		require('./logics/makeStruct')(response);
 		require('./logics/generateConfig')(response);
 
 		log(chalk.magenta('\nУстановка завершена'));
-		log(chalk.cyanBright(' npm run build - сборка бандла'));
-		log(chalk.cyanBright(' npm run serve - запуск dev-сервера и автосборки файлов по изменению'));
+		log(chalk.blue(' npm run build - сборка бандла'));
+		log(chalk.blue(' npm run serve - запуск dev-сервера и автосборки файлов по изменению'));
 	} catch (e) {
 		log(chalk.red(` - ${e.message} \r\n Stack: \r\n ${e.stack}`));
 		process.exit(1);

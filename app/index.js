09babcebd5934ed242ae38e399389cbc2243a325
d0kur0 <dev.dokuro@gmail.com>
2019-09-15 16:39:33 +0300

    Add check dir module

--- app/index.js
+++ app/index.js
@@ -1,23 +1,27 @@
 #!/usr/bin/env node
 
 const prompts = require('prompts');
-const log = console.log;
-const chalk = require('chalk');
+const notifications = require('./generator/helpers/notifications');
+const checkCurrentDir = require('./check_current_dir');
 
 (async () => {
+	if (!await checkCurrentDir()) {
+		process.exit(1);
+	}
+
 	const response = await prompts(require('./questions'));
 
 	try {
-		await require('./logics/initNPM')(response.name);
-		await require('./logics/installPackages')(response);
-		require('./logics/makeStruct')(response);
-		require('./logics/generateConfig')(response);
+		await require('./generator/modules/init_npm')(response.name);
+		//await require('./generator/modules/install_packages')(response);
+		//require('./generator/modules/make_project_structure')(response);
+		//require('./generator/modules/сonfig_generator')(response);
 
-		log(chalk.magenta('\nУстановка завершена'));
-		log(chalk.blue(' npm run build - сборка бандла'));
-		log(chalk.blue(' npm run serve - запуск dev-сервера и автосборки файлов по изменению'));
+		notifications.success('\nУстановка завершена');
+		notifications.success(' 一 npm run build - сборка бандла');
+		notifications.success(' 一 npm run serve - запуск dev-сервера и автосборки файлов по изменению');
 	} catch (e) {
-		log(chalk.red(` - ${e.message} \r\n Stack: \r\n ${e.stack}`));
+		notifications.error(e.message);
 		process.exit(1);
 	}
 })();

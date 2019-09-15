efdaf3bc0e1d412ab5e921b30d150a384815f1d2
d0kur0 <dev.dokuro@gmail.com>
2019-09-15 16:38:09 +0300

    Отрефакторил код, вынес проверку доступности папки в отдельный модуль

--- app/generator/modules/init_npm.js
+++ app/generator/modules/init_npm.js
@@ -1,30 +1,29 @@
 const fs = require('fs');
 const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const exec =  util.promisify(require('child_process').exec);
 const spinner = require('../helpers/spinner');
+const packageJsonPath = './package.json';
 
 module.exports = async (projectName) => {
-    spinner.start("Инициализация NPM");
-
-    let isAllowedDir = fs.readdirSync(process.cwd());
-
-    if (!isAllowedDir) {
-        throw new Error("Не удалось прочитать текущую директорию");
-    } else if (isAllowedDir.length) {
-        throw new Error("Текущая директория не пуста");
-    }
+    spinner.start("Initializing NPM");
 
     if (!await exec('npm init --yes')) {
-        throw new Error("Не удалось инициализировать NPM");
+        throw new Error("Failed to initialize NPM");
     }
 
-    const packageJson = JSON.parse(fs.readFileSync('packages'));
+    const packageJson = JSON.parse(fs.readFileSync(
+        packageJsonPath,
+        'utf-8'
+    ));
 
     packageJson.name = projectName;
     packageJson.author = require("os").userInfo().username;
     packageJson.scripts = require('../package.json_config/scripts');
 
-    fs.writeFileSync('packages', JSON.stringify(packageJson));
+    fs.writeFileSync(
+        packageJsonPath,
+        JSON.stringify(packageJson)
+    );
 
     spinner.stop();
 };

2a1d33ec3e67e6d828c4f034df901775f2d958a4
d0kur0 <dev.dokuro@gmail.com>
2019-09-18 00:10:22 +0300

    Добавил секцию scripts

--- app/buildSchema/index.js
+++ app/buildSchema/index.js
@@ -1,4 +1,9 @@
 module.exports = {
 	sourcePath: './src/',
-	destinationPath: './dist/'
+	destinationPath: './dist/',
+
+	scripts: {
+		build: 'cross-env NODE_ENV=production gulp build',
+		serve: 'cross-env NODE_ENV=development gulp server'
+	}
 };
